ab07b649e947e6d2497e708b204e979461d36645
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-24 22:25:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,11 +6,3 @@ end
 
 #greet_characters(["Hoggle", "Ludo", "Sir Didymus"])
 
-def list_dwarves(array)
-  array.each do |name|
-  index = 0 
-    while index < array.count do
-      number = index + 1
-      puts number + ". " + name
-  end
-end
